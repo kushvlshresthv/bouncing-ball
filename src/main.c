60f0be5357ba798c2a85d91519cc8a51db3d12ab
kushvlshresthv <kushvlshresthv@gmail.com>
2025-06-11 19:44:15 +0545

    added the ball movement

--- src/main.c
+++ src/main.c
@@ -7,16 +7,38 @@
 
 #define WINDOW_WIDTH 800
 #define WINDOW_HEIGHT 800
+#define ACC_GRAVITY 10
 
+
+
+//variables:
 SDL_Surface* global_surface = NULL;
 SDL_Window* global_window = NULL;
+
+typedef struct {
+  float center_x;
+  float center_y;
+  float radius;
+  float velocity_x;
+  float velocity_y;
+} Circle;
 /* SDL_Renderer* global_renderer = NULL; */
 
 
 
 
+
 //function definitions:
-void createCircle(int, int , int);
+void createCircle(Circle*);
+void step(Circle*);
+
+
+
+
+
+
+
+
 
 bool init() {
   if(SDL_Init(SDL_INIT_VIDEO) < 0) {
@@ -31,6 +53,7 @@ bool init() {
     return false;
   }
   global_surface = SDL_GetWindowSurface(global_window);
+
 
 
 
@@ -70,6 +93,7 @@ int main(int argc, char* argv[]) {
 
   //variables:
   bool quit = false;
+  Circle *circle = &(Circle){.center_x = 300, .center_y = 300, .radius = 100, .velocity_x = 30, .velocity_y = 10};
 
 
 
@@ -92,13 +116,16 @@ int main(int argc, char* argv[]) {
     }
 
 
+    //calculate the position of the circle
+    step(circle);
+
+    //create the circle
+    createCircle(circle);
 
 
-
-    //update the window
-    createCircle(300, 300, 100);
     SDL_UpdateWindowSurface(global_window);
   }
+
 
 
   close();
@@ -106,23 +133,46 @@ int main(int argc, char* argv[]) {
 }
 
 
+void step(Circle* circle) {
+    //clear the screen
+    int width, height;
+    SDL_GetWindowSize(global_window, &width, &height);
+    SDL_FillRect(global_surface, &(SDL_Rect){0, 0, width, height}, 0x000000);
 
-void createCircle(int x, int y, int radius) {
-  SDL_FillRect(global_surface, &(SDL_Rect){x-radius, y-radius, radius*2, radius*2},0xffffff);
-  SDL_FillRect(global_surface, &(SDL_Rect){x, y, 2, 2}, 0x89854b);
+
+    //update the position of the ball
+    circle->center_y += circle->velocity_y*0.01;
+    circle->velocity_y += ACC_GRAVITY*0.01;
+    circle->center_x += circle->velocity_x*0.01;
+
+    if(circle->center_y + circle->radius > height || circle->center_y - circle->radius <0) {
+     circle->velocity_y = -(circle->velocity_y);
+    }
+
+    if(circle->center_x + circle->radius > height || circle->center_x - circle->radius <0) {
+     circle->velocity_x = -(circle->velocity_x);
+    }
+
+}
 
 
-  int left_boundry = x-radius;
-  int right_boundry = x+radius;
-  int top_boundry = y - radius;
-  int bottom_boundry = y + radius;
+
+void createCircle(Circle *circle) {
+
+  //defining the boundries of the circle
+  int left_boundry = circle->center_x-circle->radius;
+  int right_boundry = circle->center_x+circle->radius;
+  int top_boundry = circle->center_y - circle->radius;
+  int bottom_boundry = circle->center_y + circle->radius;
+
+
+  //rendering the circle pixel by pixel
   for(int horizontal = left_boundry; horizontal <= right_boundry; horizontal++) {
     for(int vertical = top_boundry; vertical <= bottom_boundry; vertical++) {
-      int distance_from_center_squared = (horizontal - x)*(horizontal - x) +(vertical - y)*(vertical-y);
-      if((distance_from_center_squared) <= (radius*radius)) {
+      int distance_from_center_squared = (horizontal - circle->center_x)*(horizontal - circle->center_x) +(vertical - circle->center_y)*(vertical-circle->center_y);
+      if((distance_from_center_squared) <= (circle->radius*circle->radius)) {
         SDL_FillRect(global_surface, &(SDL_Rect){horizontal, vertical, 1, 1}, 0x89854b);
       }
     }
   }
-
 }
